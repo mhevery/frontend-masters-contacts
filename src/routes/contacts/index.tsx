d308e781ae0e78e4a23a8f8aae598514d084bc9c
Miško Hevery <misko@hevery.com>
2022-11-30 13:49:10 -0600

    checkpoint 1

--- src/routes/contacts/index.tsx
+++ src/routes/contacts/index.tsx
@@ -1,4 +1,9 @@
-import { component$, Resource, useStylesScoped$ } from "@builder.io/qwik";
+import {
+  component$,
+  Resource,
+  useSignal,
+  useStylesScoped$,
+} from "@builder.io/qwik";
 import { RequestHandler, useEndpoint } from "@builder.io/qwik-city";
 import { Contact, CONTACTS } from "./fake-db";
 import CSS from "./index.css?inline";
@@ -11,22 +16,34 @@ export const onGet: RequestHandler<Contact[]> = async () => {
 export default component$(() => {
   useStylesScoped$(CSS);
   const endpoint = useEndpoint<typeof onGet>();
+  const filter = useSignal("");
   return (
     <div>
       <h1>Contacts</h1>
-      <input placeholder="Search" />
+      <input
+        placeholder="Search"
+        onInput$={(event) => {
+          filter.value = (event.target as HTMLInputElement).value;
+        }}
+      />
       <Resource
         value={endpoint}
         onPending={() => <div>Loading...</div>}
         onResolved={(contacts) => {
           return (
             <ul>
-              {contacts.map((contact) => (
-                <li>
-                  <img src={contact.avatar} />
-                  {contact.name}
-                </li>
-              ))}
+              {contacts
+                .filter(
+                  (c) =>
+                    c.name.toLowerCase().indexOf(filter.value.toLowerCase()) >
+                    -1
+                )
+                .map((contact) => (
+                  <li>
+                    <img src={contact.avatar} />
+                    {contact.name}
+                  </li>
+                ))}
             </ul>
           );
         }}
